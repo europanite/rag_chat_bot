27bc82f0bc63c098a0064fbc45ae6adbd4e9765d
europanite <europanite@gmail.com>
2026-04-26 08:45:02 +0900

    fix/android

--- frontend/app/components/SettingsBar.tsx
+++ frontend/app/components/SettingsBar.tsx
@@ -93,6 +93,7 @@ export default function SettingsBar({ title = "GOODDAY YOKOSUKA" }: Props) {
               justifyContent: "center",
             }}
           >
+            <Btn title="Guides" onPress={() => openUrl(GUIDES_URL)} />
             <Btn title="Contact" onPress={() => openUrl(CONTACT_URL)} />
           </View>
         </View>
